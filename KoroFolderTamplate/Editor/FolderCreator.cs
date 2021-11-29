76ff0ee9e948149202c79c2120ce9b9d5e2464bb
Grigory Krasnov <51174663+filgreen3@users.noreply.github.com>
2021-11-29 23:33:11 +0300

    init

---
+++ KoroFolderTamplate/Editor/FolderCreator.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace Koro.FolderCreator
+{
+
+    public class FolderCreator
+    {
+        [MenuItem("Tools/Koro/FolderTamplate/Create Tamplate Folders")]
+        static void CreateTamplateFolders()
+        {
+            CreateFolder("Materials");
+            CreateFolder("Mashes");
+            CreateFolder("Plugins");
+            CreateFolder("Prefabs");
+            CreateFolder("Resources");
+            CreateFolder("Scenes");
+            CreateFolder("Sprites");
+            CreateFolder("Audio");
+            CreateFolder("Animations");
+            CreateFolder("StorePackages");
+            CreateFolder("Scripts");
+
+            CreateFolder("UI", "Assets/Scripts");
+            CreateFolder("Ads", "Assets/Scripts");
+            CreateFolder("Balance", "Assets/Scripts");
+            CreateFolder("Utility", "Assets/Scripts");
+            CreateFolder("World", "Assets/Scripts");
+            CreateFolder("Test", "Assets/Scripts");
+        }
+
+
+        private static void CreateFolder(string name, string parent = "Assets")
+        {
+            if (!AssetDatabase.IsValidFolder($"{parent}/{name}"))
+                AssetDatabase.CreateFolder(parent, name);
+        }
+    }
+}
